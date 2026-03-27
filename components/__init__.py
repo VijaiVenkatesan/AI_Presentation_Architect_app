1758f9b272cd4bb31880ae636c17deda2780ab40
VijaiVenkatesan <92427958+VijaiVenkatesan@users.noreply.github.com>
2026-03-27 16:34:59 +0530

    Update __init__.py

--- components/__init__.py
+++ components/__init__.py
@@ -1,6 +1,13 @@
+"""Components package initialization"""
 from .sidebar import render_sidebar
-from .editor import render_editor
-from .preview import render_preview
+from .editor import render_slide_editor
+from .preview import display_all_slides_preview, render_slide_preview
 from .chat_interface import render_chat_interface
 
-__all__ = ['render_sidebar', 'render_editor', 'render_preview', 'render_chat_interface']
+__all__ = [
+    'render_sidebar',
+    'render_slide_editor',
+    'display_all_slides_preview',
+    'render_slide_preview',
+    'render_chat_interface'
+]
