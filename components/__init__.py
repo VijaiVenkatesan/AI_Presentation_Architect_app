121c689fc8ccdb03720298c0116e1b6fcb3da22b
VijaiVenkatesan <92427958+VijaiVenkatesan@users.noreply.github.com>
2026-03-28 20:27:26 +0530

    Delete components directory

--- components/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Components package"""
-from .sidebar import render_sidebar
-from .editor import render_slide_editor
-from .preview import display_all_slides_preview, render_slide_preview
-from .chat_interface import render_chat_interface
-
-__all__ = ['render_sidebar', 'render_slide_editor', 'display_all_slides_preview', 'render_slide_preview', 'render_chat_interface']
