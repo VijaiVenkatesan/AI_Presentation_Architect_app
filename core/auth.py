ea2f5c547d9ecc4d28bad88688f6b9396023fbc4
VijaiVenkatesan <92427958+VijaiVenkatesan@users.noreply.github.com>
2026-03-28 20:48:36 +0530

    Update auth.py

--- core/auth.py
+++ core/auth.py
@@ -1,1 +1,23 @@
+import sqlite3
+import hashlib
 
+def hash_password(pw):
+    return hashlib.sha256(pw.encode()).hexdigest()
+
+def signup(username, password):
+    conn = sqlite3.connect("app.db")
+    c = conn.cursor()
+    try:
+        c.execute("INSERT INTO users VALUES (NULL, ?, ?)",
+                  (username, hash_password(password)))
+        conn.commit()
+        return True
+    except:
+        return False
+
+def login(username, password):
+    conn = sqlite3.connect("app.db")
+    c = conn.cursor()
+    c.execute("SELECT * FROM users WHERE username=? AND password=?",
+              (username, hash_password(password)))
+    return c.fetchone()
